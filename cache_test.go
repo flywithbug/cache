0ad763358e2a89059b336be787551559583540ae
flywithbug <flywithbug@gmail.com>
2019-07-17 16:19:52 +0800

    gitignore

--- cache_test.go
+++ cache_test.go
@@ -3,12 +3,16 @@ package cache
 import (
 	`fmt`
 	`testing`
+	"time"
 )
 
 func TestCache_Add(t *testing.T) {
 	c := New(DefaultExpiration,DefaultExpiration)
-	c.Set("foo","bar",DefaultExpiration)
+	c.Set("foo","bar",time.Second *2)
 	c.Set("baz", 42, NoExpiration)
+	fmt.Println(c.items)
+
+	time.Sleep(time.Second *6)
 
 	fmt.Println(c.items)
 }
